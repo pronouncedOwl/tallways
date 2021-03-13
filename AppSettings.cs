a62246f6108da979af2b7f05ee6a5e9313852770
Andrew Showell <pronouncedowl@andrews-mbp.lan>
2021-03-13 14:12:27 -0600

    Init

---
+++ AppSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace RCB.TypeScript
+{
+    public class AppSettings
+    {
+        public static AppSettings Default { get; }
+
+        protected AppSettings()
+        {
+        }
+
+        static AppSettings()
+        {
+            Default = new AppSettings();
+        }
+
+        public bool IsDevelopment => Program.EnvironmentName == "Development";
+    }
+}
